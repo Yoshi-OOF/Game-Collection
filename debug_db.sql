1799e9c220f6126b552b83cd7f59f777476a70af
DrakHardL <tangi.beneat@gmail.com>
2024-12-10 10:32:24 +0100

    fix db

--- debug_db.sql
+++ debug_db.sql
@@ -60,9 +60,9 @@ INSERT INTO JEU VALUES
     (4, "Baldur's Gate III", "Larian Studios", "https://baldursgate3.game/", "https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg", "2024-10-12");
 
 INSERT INTO PLATEFORME VALUES
-    (1, "nintendo");
-    (2, "playstation");
-    (3, "xbox");
+    (1, "nintendo"),
+    (2, "playstation"),
+    (3, "xbox"),
     (4, "pc");
 
 INSERT INTO POSSEDE VALUES
