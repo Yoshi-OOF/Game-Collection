58e7cc20d30edd080bac14520dcbf31d8eff7def
Maxime Jousse <maximejousse720@gmail.com>
2024-12-10 11:02:07 +0100

    Update BibliotequeController.php

--- Controllers/BibliotequeController.php
+++ Controllers/BibliotequeController.php
@@ -1,14 +1,9 @@
 <?php
-class BibliotequeController {
-    private $model;
+require_once 'Models/BibliotequeModel.php';
 
-    public function __construct($model) {
-        $this->model = $model;
-    }
-
-    public function afficher_jeux() {
-        $jeux = $this->model->get_jeux();
-        include 'Views/BibliotequeView.php';
-    }
+function afficher_jeux() {
+    $model = new BibliotequeModel();
+    $jeux = $model->get_jeux();
+    include 'Views/BibliotequeView.php';
 }
 ?>
