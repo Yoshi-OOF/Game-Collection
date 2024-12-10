3cf68df174381716b860cba4e1325b7a1e6a5c8d
Maxime Jousse <maximejousse720@gmail.com>
2024-12-10 10:11:40 +0100

    refactor navigation

--- Views/AccueilView.php
+++ Views/AccueilView.php
@@ -22,14 +22,7 @@ $username = "Guillaume";
 <body>
     <header class="header">
         <div class="logo">Logo</div>
-        <nav class="nav">
-            <ul>
-                <li><a href="accueil.php">Ma Bibliothèque</a></li>
-                <li><a href="addGame.php">Ajouter un Jeu</a></li>
-                <li><a href="classement.php">Classement</a></li>
-                <li><a href="profile.php">Profil</a></li>
-            </ul>
-        </nav>
+        @include('Views/Navigation.php')
     </header>
     
     <main>
---
+++ Views/NavigationView.php
@@ -0,0 +1,8 @@
+<nav class="nav">
+            <ul>
+                <li><a href="accueil.php">Ma Bibliothèque</a></li>
+                <li><a href="addGame.php">Ajouter un Jeu</a></li>
+                <li><a href="classement.php">Classement</a></li>
+                <li><a href="profile.php">Profil</a></li>
+            </ul>
+        </nav>
