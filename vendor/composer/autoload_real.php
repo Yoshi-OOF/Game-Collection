5fdc07ec91cc1ab6d7ce13470f1ffc9c4aaaf39e
Yoshi <andreolienzo@gmail.com>
2024-12-10 10:27:51 +0100

    ok mais pourquoi

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitebab99d7d2debc7e58e725a36503fe01
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitebab99d7d2debc7e58e725a36503fe01', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitebab99d7d2debc7e58e725a36503fe01', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitebab99d7d2debc7e58e725a36503fe01
         call_user_func(\Composer\Autoload\ComposerStaticInitebab99d7d2debc7e58e725a36503fe01::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitebab99d7d2debc7e58e725a36503fe01::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
