d536ead057c1a4fccfc0022e4af4f7a1591a4d75
Yoshi <andreolienzo@gmail.com>
2024-12-10 11:18:04 +0100

    Rename

--- Views/NavigationView.php
+++ Views/NavigationView.php
@@ -1,9 +1,9 @@
 <nav class="nav">
     <ul>
         <img src="../Assets/img/logo" alt="logo" >
-        <li><a href="Accueil.php">Ma Bibliothèque</a></li>
+        <li><a href="BibliotequeView.php">Ma Bibliothèque</a></li>
         <li><a href="AjouterJeuView.php">Ajouter un Jeu</a></li>
-        <li><a href="Classement.php">Classement</a></li>
+        <li><a href="ClassementView.php">Classement</a></li>
         <li><a href="ProfileView.php">Profil</a></li>
     </ul>
 </nav>
