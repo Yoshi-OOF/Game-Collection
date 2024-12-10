98ae497a0d1c740a6d6b22c8c92d21fe0a6b469c
Yoshi <andreolienzo@gmail.com>
2024-12-10 09:52:58 +0100

    Baguette

---
+++ Classes/DataConstructor.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'Data.php';
+
+$host = 'localhost';
+$dbname = 'gamecollection';
+$username = 'root';
+$password = '';
+
+$data = new Data($host, $dbname, $username, $password);
+?>
