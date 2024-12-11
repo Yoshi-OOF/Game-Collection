f4566895d793f1d05b92d17874d3846cec8db1b8
DrakHardL <tangi.beneat@gmail.com>
2024-12-11 10:36:00 +0100

    add desc to debug data

--- debug_db.sql
+++ debug_db.sql
@@ -55,10 +55,10 @@ INSERT INTO COMPTE VALUES
     (4, "enzo", "Andreoli", "enzo.nom@gmail.com", "$2y$10$PVo5Mm5hvKnolSlal0K9CelwcsYaQIDi4mSmy16VmecMLbUTQM5Aq");
 
 INSERT INTO JEU VALUES
-    (1, "GTA V", "Rockstar Games", "https://www.rockstargames.com/fr/gta-v", "https://m.media-amazon.com/images/I/71P3UUlwBdL._AC_UF1000,1000_QL80_.jpg", "2024-10-12"),
-    (2, "Red Dead Redemption II", "Rockstar Games", "https://store.steampowered.com/app/1174180/Red_Dead_Redemption_2/", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFKqEd7cIt_4DOPwyDQ7W8LBfMw4DYI95sbg&s", "2024-10-12"),
-    (3, "The Legend of Zelda: Breath of the Wild", "Nintendo", "https://www.nintendo.com/fr-fr/Jeux/Jeux-Nintendo-Switch/The-Legend-of-Zelda-Breath-of-the-Wild-1173609.html?srsltid=AfmBOoocht0yhIbQdqIYktoHsQs8D6aNt5wEeWOkG6WouYRev8FXR_Z_", "https://static.posters.cz/image/1300/toiles-the-legend-of-zelda-breath-of-the-wild-sunset-i111061.jpg", "2024-10-12"),
-    (4, "Baldur's Gate III", "Larian Studios", "https://baldursgate3.game/", "https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg", "2024-10-12");
+    (1, "GTA V", "Rockstar Games", "un bon vieux jeu de plus de 10 ans.", "https://www.rockstargames.com/fr/gta-v", "https://m.media-amazon.com/images/I/71P3UUlwBdL._AC_UF1000,1000_QL80_.jpg", "2024-10-12"),
+    (2, "Red Dead Redemption II", "Rockstar Games", "Un simulateur de cowboy dans un farwest futuristque.", "https://store.steampowered.com/app/1174180/Red_Dead_Redemption_2/", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFKqEd7cIt_4DOPwyDQ7W8LBfMw4DYI95sbg&s", "2024-10-12"),
+    (3, "The Legend of Zelda: Breath of the Wild", "Nintendo", "retrouve cette conne qui c'est fait encore enleve", "https://www.nintendo.com/fr-fr/Jeux/Jeux-Nintendo-Switch/The-Legend-of-Zelda-Breath-of-the-Wild-1173609.html?srsltid=AfmBOoocht0yhIbQdqIYktoHsQs8D6aNt5wEeWOkG6WouYRev8FXR_Z_", "https://static.posters.cz/image/1300/toiles-the-legend-of-zelda-breath-of-the-wild-sunset-i111061.jpg", "2024-10-12"),
+    (4, "Baldur's Gate III", "Larian Studios", "adapation de l'univers de Donjon et Dragon dans un jeu multijoueur", "https://baldursgate3.game/", "https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg", "2024-10-12");
 
 INSERT INTO PLATEFORME VALUES
     (1, "nintendo"),
