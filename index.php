a899cc03dbc24a34ac29a349c8bea5724dd21acc
Yoshi <andreolienzo@gmail.com>
2024-12-16 15:46:26 +0100

    Push workflow

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
 
+// Load dependencies
+require 'vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require 'db.php';
+
 if (isset($_SESSION['user_id'])) {
     header("Location: Views/accueil.php");
     exit();
