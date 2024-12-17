c3e0d0bb128e10a2d5fa12a37c5afdb8c8387909
Yoshi <andreolienzo@gmail.com>
2024-12-17 10:24:32 +0100

    update

--- Classes/DataConstructor.php
+++ Classes/DataConstructor.php
@@ -1,10 +1,11 @@
 <?php
 include_once 'Data.php';
 
-$host = 'localhost';
-$dbname = 'gamecollection';
-$username = 'root';
-$password = '';
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
 
 $data = new Data($host, $dbname, $username, $password);
 ?>
