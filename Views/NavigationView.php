6bb302ac7e2a2c9558e628b8e9faa937d952c50e
Maxime Jousse <maximejousse720@gmail.com>
2024-12-17 11:25:58 +0100

    Start of profil

--- Views/NavigationView.php
+++ Views/NavigationView.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <img src="../Assets/img/logo" alt="logo" >
+        <img src="../Assets/img/logo.png" alt="logo" >
         <li><a href="index.php?action=bibliotheque">Ma Bibliothèque</a></li>
         <li><a href="index.php?action=ajouterJeu">Ajouter un Jeu</a></li>
         <li><a href="index.php?action=classement">Classement</a></li>
