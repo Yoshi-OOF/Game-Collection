50bd812b0f6a4686555dee2a9b9238e66b186d18
Maxime Jousse <maximejousse720@gmail.com>
2024-12-17 10:54:13 +0100

    Update BibliothequeView.php

--- Views/BibliothequeView.php
+++ Views/BibliothequeView.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Barlow' rel='stylesheet'>
-    <link rel="stylesheet" href="../Css/Bibliotheque.css">
+    <link rel="stylesheet" href="Css/Bibliotheque.css">
     <title>Accueil - Game Collection</title>
 </head>
 <body>
