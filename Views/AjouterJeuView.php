65132be1a1350e49ee6b4b3c23dd2b348b789f36
Yoshi <andreolienzo@gmail.com>
2024-12-17 10:52:04 +0100

    try

--- Views/AjouterJeuView.php
+++ Views/AjouterJeuView.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter un jeu</title>
-    <link rel="stylesheet" href="../Css/AjouterJeu.css">
+    <link rel="stylesheet" href="Css/AjouterJeu.css">
 </head>
 <body>
     <header class="header">
--- Views/ProfileView.php
+++ Views/ProfileView.php
@@ -16,7 +16,7 @@ $username = $_SESSION['username'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil - Game Collection</title>
-    <link rel="stylesheet" href="../Css/Profile.css">
+    <link rel="stylesheet" href="Css/Profile.css">
 </head>
 <body>
     <header>
