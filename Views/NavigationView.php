b06af4b40b696edd73a30a9d58b8ab64e545785f
DrakHardL <tangi.beneat@gmail.com>
2025-01-06 14:54:07 +0100

    add style for navigation bar

--- Views/NavigationView.php
+++ Views/NavigationView.php
@@ -1,11 +1,24 @@
-<link rel="stylesheet" href="Css/Navigation.css">
-<nav>
-    <ul>
-        <img src="../Assets/img/logo.png" alt="logo" >
-        <li><a href="index.php?action=bibliotheque">Ma Bibliothèque</a></li>
-        <li><a href="index.php?action=ajouterJeu">Ajouter un Jeu</a></li>
-        <li><a href="index.php?action=classement">Classement</a></li>
-        <li><a href="index.php?action=profil">Profil</a></li>
-        <li><a href="index.php?action=logout">Déconnexion</a></li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Barlow' rel='stylesheet'>
+    <link rel="stylesheet" href="Css/Navigation.css">
+    <title>Header</title>
+</head>
+
+<body>
+    <div class="logo">
+        <img src="../Assets/img/logo.png" alt="logo">
+    </div>
+    <div class="nav-links">
+        <a href="index.php?action=bibliotheque">Ma Bibliothèque</a>
+        <a href="index.php?action=ajouterJeu">Ajouter un Jeu</a>
+        <a href="index.php?action=classement">Classement</a>
+        <a href="index.php?action=profil">Profil</a>
+    </div>
+</body>
+
+</html>
