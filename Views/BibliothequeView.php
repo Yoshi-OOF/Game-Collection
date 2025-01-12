0320d2e99036ba2f9a3a275a62b0f61180be6e6e
DrakHardL <tangi.beneat@gmail.com>
2025-01-12 17:43:58 +0100

    game card

--- Views/BibliothequeView.php
+++ Views/BibliothequeView.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Barlow' rel='stylesheet'>
     <link rel="stylesheet" href="Css/Bibliotheque.css">
+    <link rel="stylesheet" href="Css/ItemCard.css">
     <title>Accueil - Game Collection</title>
 </head>
 
@@ -25,28 +26,31 @@
         <h2>Mes jeux</h2>
 
         <?php if (!empty($jeux)): ?>
-
-            <div class="game-window">
-                <div class="game-display">
-
-                    <?php foreach ($jeux as $jeu): ?>
+            <div class="games-container">
+                <?php foreach ($jeux as $jeu): ?>
+                    <div class="game">
                         <a href="index.php?action=modifierJeu&id_jeu=<?php echo $jeu['id_jeu']; ?>">
-                            <div class="game-card">
-                                <img src="<?php echo htmlspecialchars($jeu['url_couverture_jeu']); ?>" alt="RDR" height="150px">
 
-                                <div class="card-info">
-                                    <p><?php echo $jeu["nom_jeu"]; ?></p>
-                                    <p><?php echo $jeu["temps_jeu"]; ?> h</p>
-                                </div>
-                                <div class="device" style="display: flex; gap: 10px;">
-                                    <?php foreach ($jeux_par_plateforme[$jeu['id_jeu']] as $platforme): ?>
-                                        <p><?php echo $platforme['nom_plateforme']; ?></p>
-                                    <?php endforeach; ?>
-                                </div>
+                            <img src="<?php echo htmlspecialchars($jeu['url_couverture_jeu']); ?>" alt="<?php echo "Nom du jeu"; ?>">
+
+                            <div class="game-info">
+                                <p><?php echo $jeu["nom_jeu"]; ?></p>
+                                <p>
+                                    <?php if (isset($jeu["temps_jeu"])): ?>
+                                        <?php echo $jeu["temps_jeu"]; ?>h
+                                    <?php else: ?>
+                                        0h
+                                    <?php endif; ?>
+                                </p>
+                            </div>
+                            <div class="game-devices">
+                                <?php foreach ($jeux_par_plateforme[$jeu['id_jeu']] as $platforme): ?>
+                                    <p class="game-device"><?php echo $platforme['nom_plateforme']; ?></p>
+                                <?php endforeach; ?>
                             </div>
                         </a>
-                    <?php endforeach; ?>
-                </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
         <?php else: ?>
             <div class="error-no-games">
