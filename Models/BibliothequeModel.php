14c19ed8511f062b29558196c74dbecc061f399e
Yoshi <andreolienzo@gmail.com>
2025-01-12 16:16:53 +0100

    Fix table name

--- Models/BibliothequeModel.php
+++ Models/BibliothequeModel.php
@@ -19,7 +19,7 @@ class BibliothequeModel {
 
     public function get_platformes($id_jeu){
         $query = "SELECT nom_plateforme FROM PLATEFORME
-                    INNER JOIN COMPATIBLE ON Plateforme.id_plateforme = COMPATIBLE.id_plateforme
+                    INNER JOIN COMPATIBLE ON PLATEFORME.id_plateforme = COMPATIBLE.id_plateforme
                     WHERE COMPATIBLE.id_jeu = :id_jeu";
         $params = [':id_jeu' => $id_jeu];
         return $this->data->query($query, $params);
