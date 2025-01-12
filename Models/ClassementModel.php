499e8975b334d40d3330761a9dd13c67aa9ef906
Yoshi <andreolienzo@gmail.com>
2025-01-12 16:22:15 +0100

    LES NOMS DE TABLES EN MAJUSCULES!!!!!

--- Models/ClassementModel.php
+++ Models/ClassementModel.php
@@ -9,8 +9,8 @@ class ClassementModel {
 
     public function get_classement() {
         $query = "SELECT * FROM POSSEDE 
-            INNER JOIN Jeu ON JEU.id_jeu = POSSEDE.id_jeu 
-            INNER JOIN Compte ON compte.id_compte = POSSEDE.id_compte ORDER BY POSSEDE.temps_jeu DESC";
+            INNER JOIN JEU ON JEU.id_jeu = POSSEDE.id_jeu 
+            INNER JOIN COMPTE ON compte.id_compte = POSSEDE.id_compte ORDER BY POSSEDE.temps_jeu DESC";
         return $this->data->query($query);
     }
 }
--- Models/ModifierJeuModel.php
+++ Models/ModifierJeuModel.php
@@ -8,19 +8,19 @@ class ModifierJeuModel {
     }
 
     public function getJeu($id) {
-        $query = "SELECT * FROM jeu INNER JOIN possede ON jeu.id_jeu = possede.id_jeu WHERE jeu.id_jeu = :id AND possede.id_compte = :id_compte";  
+        $query = "SELECT * FROM JEU INNER JOIN POSSEDE ON jeu.id_jeu = POSSEDE.id_jeu WHERE jeu.id_jeu = :id AND POSSEDE.id_compte = :id_compte";  
         $params = [':id' => $id , ':id_compte' => $_SESSION['compte']['id_compte']];
         return $this->data->query($query, $params);
     }
 
     public function updateJeu($id, $temps) {
-        $query = "UPDATE possede SET temps_jeu = :temps WHERE id_jeu = :id AND id_compte = :id_compte";
+        $query = "UPDATE POSSEDE SET temps_jeu = :temps WHERE id_jeu = :id AND id_compte = :id_compte";
         $params = [':temps' => $temps, ':id' => $id , ':id_compte' => $_SESSION['compte']['id_compte']];
         return $this->data->execute($query, $params);
     }
     
     public function deleteJeu($id) {
-        $query = "DELETE FROM possede WHERE id_jeu = :id";
+        $query = "DELETE FROM POSSEDE WHERE id_jeu = :id";
         $params = [':id' => $id];
         return $this->data->execute($query, $params);
     }
